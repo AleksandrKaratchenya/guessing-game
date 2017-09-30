5fedea3d55a497545f207258c167558a6661f8f0
Aleksandr Karatchenya <sanya.karatchenya@mail.ru>
2017-09-30 16:32:22 +0300

    Change

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,18 +2,29 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+this.min=min;
+this.max=max;
+this.counter=0;
     }
 
     guess() {
 
+return Math.floor((this.max+this.min)/2)+Math.floor(this.counter*0.5);
     }
 
     lower() {
+if((this.max+this.min)%2!=0)
+this.counter++;
+const a= (this.max+this.min);
+this.max=Math.floor(a/2);
 
     }
 
     greater() {
+if((this.max+this.min)%2!=0)
+this.counter++;
+const a= Math.floor((this.max+this.min)/2);
+this.min=a;
 
     }
 }
