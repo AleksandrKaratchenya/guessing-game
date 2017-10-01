9ee2d8293e43262380f81e89d71fbe6b571640b2
Aleksandr Karatchenya <sanya.karatchenya@mail.ru>
2017-10-01 13:24:50 +0300

    Change 2

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -4,26 +4,24 @@ class GuessingGame {
     setRange(min, max) {
 this.min=min;
 this.max=max;
-this.counter=0;
     }
 
+   
     guess() {
 
-return Math.floor((this.max+this.min)/2)+Math.floor(this.counter*0.5);
-    }
+return (this.max+this.min)/2|0 +2;
+ 
+   }
 
     lower() {
-if((this.max+this.min)%2!=0)
-this.counter++;
 const a= (this.max+this.min);
-this.max=Math.floor(a/2);
+this.max=a/2;
 
     }
 
     greater() {
-if((this.max+this.min)%2!=0)
-this.counter++;
-const a= Math.floor((this.max+this.min)/2);
+const a= (this.max+this.min)/2;
+
 this.min=a;
 
     }
